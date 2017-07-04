c4cad58e209b445c37213a4911a988eb825027a3
Dmitry <AntiduPb@yandex.ru>
2017-07-04 22:56:24 +0300

    add progressbar

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <ncurses.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
 
 #include "dirslist.h"
 #include "pane.h"
@@ -66,6 +69,8 @@ int main(int argc, char** argv) {
     init_pair(1, COLOR_BLACK, COLOR_BLUE);
     // Цвет рамки.
     init_pair(2, COLOR_BLACK, COLOR_CYAN);
+    // Цвет progress bar.
+    init_pair(3, COLOR_BLACK, COLOR_MAGENTA);
 
     // Управление клавиатурой.
     keypad(stdscr, true);
@@ -139,41 +144,41 @@ int main(int argc, char** argv) {
         if (pressed_key == '\n') {
 
             // Тут запуск файла.
-            /*
-                        if ((*(mdirs->ilist + real_position))->itype == ISFILE) {
-                            char work[255];
-                            // Тут строим полный путь к запускаемому файлу.
-                            strcpy(work, getcwd(NULL, 255));
-                            strcat(work, "/");
-                            strcat(work, (*(mdirs->ilist + real_position))->name);
 
-                            //printw("\n\n %s\n", work);
-                            // Временно закрываем окно 
-                            clear();
-                            refresh();
-                            endwin();
-                            printf("\n%s\n", work);
-                            pid_t ipid;
-                            ipid = fork();
-                            if (ipid == 0) {
-                                execl(work, work, NULL);
-                                //execl("/usr/bin/ls", "/usr/bin/ls", "-al", NULL);
-                                exit(0);
-                            }
-                            // Что-то типа лога.
-                            // FILE *file_ptr = fopen("le3_log.log", "w");
-                            // fprintf(file_ptr, "\n %s - %s \n", work, "was executed.\n");
-                            // fclose(file_ptr);
-                                
-                            int statuss;
-                            wait(&statuss);
-                            getch();
-                            pressed_key = ' ';
-                            // Снова открываем окно.
-                            mwin = initscr();
-                            continue;
-                        }
-             */
+            if ((*(active_pane->dirlist->ilist + active_pane->real_position))->itype == ISFILE) {
+                char work[255];
+                // Тут строим полный путь к запускаемому файлу.
+                strcpy(work, getcwd(NULL, 255));
+                strcat(work, "/");
+                strcat(work, (*(active_pane->dirlist->ilist + active_pane->real_position))->name);
+
+                //printw("\n\n %s\n", work);
+                // Временно закрываем окно 
+                clear();
+                refresh();
+                endwin();
+                printf("\n%s\n", work);
+                pid_t ipid;
+                ipid = fork();
+                if (ipid == 0) {
+                    execl(work, work, NULL);
+                    //execl("/usr/bin/ls", "/usr/bin/ls", "-al", NULL);
+                    exit(0);
+                }
+                // Что-то типа лога.
+                // FILE *file_ptr = fopen("le3_log.log", "w");
+                // fprintf(file_ptr, "\n %s - %s \n", work, "was executed.\n");
+                // fclose(file_ptr);
+
+                int statuss;
+                wait(&statuss);
+                getch();
+                pressed_key = ' ';
+                // Снова открываем окно.
+                initscr();
+                continue;
+            }
+
 
 
             // Переходим в каталог на котором нажали ENTER
@@ -187,6 +192,33 @@ int main(int argc, char** argv) {
             active_pane->position = 0;
             active_pane->real_position = 0;
             // Обнуляем чтобы не зациклилось.
+            pressed_key = ' ';
+            continue;
+        }
+
+        if (pressed_key == KEY_F(5)) {
+            int width = 50;
+            WINDOW *progress_bar_win = newwin(6, width, getmaxy(stdscr) / 3,
+                    getmaxx(stdscr) / 2 - (width / 2));
+            box(progress_bar_win, 0, 0);
+            wrefresh(progress_bar_win);
+
+            for (int i = 0; i < 10; i++) {
+
+                wattron(progress_bar_win, COLOR_PAIR(3));
+                mvwprintw(progress_bar_win, 3, i + 2, " ");
+                wattroff(progress_bar_win, COLOR_PAIR(3));
+
+                wrefresh(progress_bar_win);
+                sleep(1);
+                
+            }
+
+            //sleep(5);
+
+            wborder(progress_bar_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+
+            wrefresh(progress_bar_win);
             pressed_key = ' ';
             continue;
         }
@@ -208,7 +240,10 @@ int main(int argc, char** argv) {
             tmp_pos += BOTTOM_BORDER;
         }
 
-        /*
+        /* Этот код работает, это "старый" вариант. Не удаляю потому что пока
+         * не знаю, действительно-ли это код сложнее второго варианта.
+         * Время покажет.
+         
                 // Вывод списка.
                 for (; tmp_pos < active_pane->dirlist->count; tmp_pos++) {
                     if (tmp_pos == active_pane->real_position) {
@@ -248,7 +283,7 @@ int main(int argc, char** argv) {
                     wprintw(active_pane->PANE_WINDOW, "\n  /%s", (*(list_item + tmp_pos))->name);
 
                 if ((*(list_item + tmp_pos))->itype == ISFILE)
-                    wprintw(active_pane->PANE_WINDOW, "\n  %s", (*(active_pane->dirlist->ilist + tmp_pos))->name);
+                    wprintw(active_pane->PANE_WINDOW, "\n  %s", (*(list_item + tmp_pos))->name);
 
                 wattroff(active_pane->PANE_WINDOW, COLOR_PAIR(1));
             } else {
